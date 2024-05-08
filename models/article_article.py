8ee039ee726c86e5b711dd7c5f35b5e8b9cd58ef
Victor <victor.onuh@st.futminna.edu.ng>
2024-05-08 17:26:00 +0100

    changes to reflect in list and kanban view on create or update record without clicking menu

--- models/article_article.py
+++ models/article_article.py
@@ -74,6 +74,23 @@ class articleManager(models.Model):
 
         return allowed_view_ids
 
+    def get_allowed_ids_with_state(self, state):
+
+        allowed_view_ids = []
+        # load ids based on logged in user
+        logged_in_user = self.env.user
+        if logged_in_user.has_group('article_manager.group_article_manager'):
+            # load all records
+            allowed_view_ids = self.env['article.article'].search([('state', '=', state)]).ids
+
+        elif logged_in_user.has_group('article_manager.group_article_reader'):
+            # load only ids associated with user
+            related_partner = self.env.user.partner_id.id
+            allowed_view_ids = self.env['article.article'].search([('assigned_to', '=', related_partner), ('state', '=', state)]).ids
+
+        return allowed_view_ids
+
+
     @api.model
     def get_modified_data(self):
 
@@ -91,9 +108,59 @@ class articleManager(models.Model):
 
     @api.model
     def search_read(self, domain=None, fields=None, offset=0, limit=None, order=None):
+        if len(domain) <= 1 and 'assigned_to' in fields:
+            domain = [['id', 'in', self.get_allowed_ids()]]
+            return super().search_read(domain=domain, fields=fields, offset=offset, limit=limit, order=order)
+        if domain[0] == '&' and domain[1][0] == 'state' and 'assigned_to' in fields:
+            state = domain[1][2]
+            domain[2] = ['id', 'in', self.get_allowed_ids_with_state(state)]
         return super().search_read(domain=domain, fields=fields, offset=offset, limit=limit, order=order)
-    
 
+    @api.model
+    def web_read_group(self, domain, fields, groupby, limit=None, offset=0, orderby=False,lazy=True, expand=False, expand_limit=None, expand_orderby=False):
+        res = super().web_read_group(domain=domain, fields=fields, groupby=groupby, limit=limit, orderby=orderby, lazy=lazy, expand_limit=expand_limit, expand_orderby=expand_orderby)
+        if 'assigned_to' in fields and groupby[0] == 'state':
+            # alter the data
+            states = []
+            state_count = []
+            ids = []
+            groups = []
+
+            state_open = self.get_allowed_ids_with_state('open')
+            if state_open:
+                states.append('open')
+                state_count.append(len(state_open))
+                ids.append(state_open)
+
+            state_reading = self.get_allowed_ids_with_state('reading')
+            if state_reading:
+                states.append('reading')
+                state_count.append(len(state_reading))
+                ids.append(state_reading)
+
+            state_read = self.get_allowed_ids_with_state('read')
+            if state_read:
+                states.append('read')
+                state_count.append(len(state_read))
+                ids.append(state_read)
+
+            state_abandon = self.get_allowed_ids_with_state('abandon')
+            if state_abandon:
+                states.append('abandon')
+                state_count.append(len(state_abandon))
+                ids.append(state_abandon)
+
+            i = 0
+            for state in states:
+                if len(states) == 1:
+                    state_dict = {'state_count': state_count[i], 'state': state, '__domain': [('state', '=', state)]}
+                else:
+                    state_dict = {'state_count': state_count[i], 'state': state, '__domain': ['&', ('state', '=', state), ('id', 'in', ids[i])]}
+                groups.append(state_dict)
+                i += 1
+            res1 = {'groups': groups, 'length': len(states)}
+            return res1
+        return res
 
     def action_move_to_reading(self):
     	self.write({'state': 'reading'})
